875ce4fb28a69d7abfdfa10bb485f85b740a3050
Arvind Kumar <thakurarvindkr10@gmail.com>
2024-01-06 15:08:59 +0530

    added header comonent

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import { Home, About, Profile, SignIn, SignUp } from "./pages";
+import Header from "./components/Header";
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </>
   );
 }
