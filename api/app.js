5de0e005169e304121bebbdd36cf22382dfe7d35
Arvind Kumar <thakurarvindkr10@gmail.com>
2024-01-11 16:02:40 +0530

    created model and test api routes

--- api/app.js
+++ api/app.js
@@ -1,12 +1,14 @@
 import express from "express";
-
+import userRoutes from "./routes/user.routes.js";
 const app = express();
-
 app.use(
   express.json({
     limit: "20kb",
   })
 );
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
+
+// routes
+app.use("/api/user", userRoutes);
 
 export { app };
